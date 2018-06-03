b9039989e8c67c49eb56d729247a1dd6d5212f96
Amar Jain <amar.jain07@gmail.com>
2018-06-03 17:08:13 +0530

    Removing vendor files

--- db/setup.go
+++ db/setup.go
@@ -1,4 +1,4 @@
-package db
+package main
 
 import(
   "os"
@@ -15,7 +15,10 @@ func main(){
   if _, err := db.Exec("CREATE TABLE IF NOT EXISTS role(id INTEGER NOT NULL AUTO_INCREMENT, type VARCHAR(20), PRIMARY KEY(`id`))"); err != nil{
     panic(err)
   }
-  if _, err := db.Exec("CREATE TABLE IF NOT EXISTS user(id INT(10) NOT NULL AUTO_INCREMENT, name VARCHAR(64), mobile VARCHAR(15),is_active BOOL DEFAULT false,role INTEGER, PRIMARY KEY (`id`), UNIQUE(`mobile`), FOREIGN KEY(`role`) REFERENCES role(`id`))"); err != nil {
+  if _, err := db.Exec("CREATE TABLE IF NOT EXISTS user(id INT(10) NOT NULL AUTO_INCREMENT, name VARCHAR(64), mobile VARCHAR(15),is_active BOOL DEFAULT false,role INTEGER DEFAULT 4, PRIMARY KEY (`id`), UNIQUE(`mobile`), FOREIGN KEY(`role`) REFERENCES role(`id`))"); err != nil {
+    panic(err)
+  }
+  if _, err := db.Exec("CREATE TABLE IF NOT EXISTS usermeta(access_token VARCHAR(128), user_id INT(10) UNIQUE, created_at DATETIME, FOREIGN KEY(`user_id`) REFERENCES user(`id`))"); err != nil {
     panic(err)
   }
   if _, err := db.Exec("CREATE TABLE IF NOT EXISTS product(id INT(10) NOT NULL AUTO_INCREMENT, name VARCHAR(64), mrp INTEGER, price INTEGER, retailer_price INTEGER, brand VARCHAR(20), category VARCHAR(20), description VARCHAR(64) NULL, image VARCHAR(512), PRIMARY KEY(`id`), UNIQUE(`name`))"); err != nil{
@@ -24,32 +27,32 @@ func main(){
   if _, err := db.Exec("CREATE TABLE IF NOT EXISTS code(id VARCHAR(128) NOT NULL, code INTEGER, PRIMARY KEY(`id`))"); err != nil{
     panic(err)
   }
-  stmt, err := db.Prepare("INSERT role SET type=?")
+  stmt, err := db.Prepare("INSERT role SET id=?, type=?")
   if err != nil{
     panic(err)
   }
-  if _, err := stmt.Exec("OWNER"); err != nil{
+  if _, err := stmt.Exec(1, "OWNER"); err != nil{
     panic(err)
   }
-  stmt, err = db.Prepare("INSERT role SET type=?")
+  stmt, err = db.Prepare("INSERT role SET id=?, type=?")
   if err != nil{
     panic(err)
   }
-  if _, err := stmt.Exec("DISTRIBUTOR"); err != nil{
+  if _, err := stmt.Exec(2, "DISTRIBUTOR"); err != nil{
     panic(err)
   }
-  stmt, err = db.Prepare("INSERT role SET type=?")
+  stmt, err = db.Prepare("INSERT role SET id=?, type=?")
   if err != nil{
     panic(err)
   }
-  if _, err := stmt.Exec("RETAILER"); err != nil{
+  if _, err := stmt.Exec(3, "RETAILER"); err != nil{
     panic(err)
   }
-  stmt, err = db.Prepare("INSERT role SET type=?")
+  stmt, err = db.Prepare("INSERT role SET id=?, type=?")
   if err != nil{
     panic(err)
   }
-  if _, err := stmt.Exec("CUSTOMER"); err != nil{
+  if _, err := stmt.Exec(4, "CUSTOMER"); err != nil{
     panic(err)
   }
   stmt, err = db.Prepare("INSERT user SET mobile=?, role=?")
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-
+	"google.golang.org/appengine"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
   "database/sql"
@@ -19,7 +19,7 @@ import (
 type User struct{
 	Name string `json:"name, omitempty"`
 	Mobile string `json:"mobile"`
-	Role string `json: role, omitempty`
+	Role string `json:"role, omitempty"`
 }
 
 type Product struct{
@@ -56,41 +56,6 @@ func main() {
 		panic(err)
 	}
 
-	router.GET("/link/:mobile", func(c *gin.Context){
-		var mobile = c.Param("mobile")
-		stmt, err := db.Prepare("insert into code(id, code) values(?, ?)")
-		defer stmt.Close()
-		if err != nil{
-			c.JSON(http.StatusOK, gin.H{"success": true, "code": 4003, "message": "Error sending sms"})
-			return
-		}
-		var random = 1000 + rand.Intn(8999)
-		hash := getMD5Hash(mobile + time.Now().String())
-		_, err = stmt.Exec(hash, random)
-		if err != nil{
-			c.JSON(http.StatusOK, gin.H{"success": true, "code": 4003, "message": "Error sending sms"})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"success": true, "code": 2000, "message": "SMS sent successfully!", "sms_code": random, "ref_id": hash})
-	})
-
-	router.GET("/verify/:id/:code", func(c *gin.Context){
-		var hash = c.Param("id")
-		paramCode, _ := strconv.Atoi(c.Param("code"))
-		var code int
-		row := db.QueryRow("select code from code where id = ?", hash)
-		err = row.Scan(&code)
-		if err != nil{
-			c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4004, "message": err.Error()})
-			return
-		}
-		if paramCode == code{
-			c.JSON(http.StatusOK, gin.H{"success": true, "code": 2000, "message": "User verified successfully!"})
-		} else {
-			c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4005, "message": "Invalid otp code"})
-		}
-	})
-
 	router.GET("/products", func(c *gin.Context){
 		var (
 			product  Product
@@ -119,7 +84,6 @@ func main() {
 	router.POST("/product", func(c *gin.Context){
 		var product Product
 		if err := c.ShouldBindJSON(&product); err == nil {
-			log.Print("Name" + product.Name)
 			stmt, err := db.Prepare("insert into product(name, mrp, price, retailer_price, brand, category, description, image) values(?, ?, ?, ?, ?, ?, ?, ?)")
 			_, err = stmt.Exec(product.Name, product.Mrp, product.Price, product.RetailerPrice, product.Brand, product.Category, product.Description, product.Image)
 			if(err != nil){
@@ -146,7 +110,7 @@ func main() {
 		if err := c.ShouldBindJSON(&user); err == nil {
 			tx, er := db.Begin()
 			if er != nil {
-					c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4000, "message": err.Error()})
+					c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4000, "message": er.Error()})
 					return
 			}
 			defer tx.Rollback()
@@ -165,7 +129,97 @@ func main() {
 		}
 	})
 
+	router.POST("/link", func(c *gin.Context){
+		var (
+			mobile = c.PostForm("mobile")
+			name = c.PostForm("name")
+		)
+		stmt, err := db.Prepare("insert into code(id, code) values(?, ?)")
+		defer stmt.Close()
+		if err != nil {
+			log.Print(err)
+			c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4003, "message": "Error sending sms"})
+			return
+		}
+		var random = 1000 + rand.Intn(8999)
+		hash := getMD5Hash(mobile + time.Now().String())
+		_, err = stmt.Exec(hash, random)
+		if err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4003, "message": "Error sending sms"})
+			return
+		}
+		go insertLoginUser(db, mobile, name)
+		log.Print("Link SMS Code " + hash + ": " + strconv.Itoa(random))
+		c.JSON(http.StatusOK, gin.H{"success": true, "code": 2000, "message": "SMS sent successfully!", "sms_code": random, "ref_id": hash})
+	})
+
+	router.POST("/verify", func(c *gin.Context){
+		var (
+			hash = c.PostForm("ref_id")
+			otpCode = c.PostForm("otp")
+			mobile = c.PostForm("mobile")
+		)
+		var code int
+		row := db.QueryRow("select code from code where id = ?", hash)
+		err = row.Scan(&code)
+		if err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4004, "message": err.Error()})
+			return
+		}
+
+		if otp, _ := strconv.Atoi(otpCode); otp == code {
+			go activateUser(db, mobile)
+			c.JSON(http.StatusOK, gin.H{"success": true, "code": 2000, "message": "User verified successfully!"})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4005, "message": "Invalid otp code"})
+		}
+	})
+
+	router.POST("/login", func(c *gin.Context){
+		var mobile = c.PostForm("mobile")
+		var random = 1000 + rand.Intn(8999)
+		token := getMD5Hash(strconv.Itoa(random) + mobile)
+		tx, err := db.Begin()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4000, "message": err.Error()})
+			return
+		}
+		defer tx.Rollback()
+		var (
+			user User
+			userId int
+		)
+		user.Mobile = mobile
+		row := tx.QueryRow("select u.id, r.type, u.name from user u inner join role r on u.role = r.id where u.mobile = ?", mobile)
+		err = row.Scan(&userId, &user.Role, &user.Name)
+		if err == nil{
+			_, err = tx.Exec("insert into usermeta(access_token, user_id, created_at) values(?, ?, ?) on duplicate key update access_token=?", token, userId, time.Now(), token)
+			if err != nil{
+				c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4006, "message": "Error Logging In"})
+				return
+			}
+			tx.Commit()
+			c.Header("X-Nothing", token)
+			c.JSON(http.StatusOK, gin.H{"success": true,"code": 2000,"message": "Logged In successfully!","user": user})
+			return
+		}
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "code": 4006, "message": "Error Logging In"})
+	})
+
 	router.Run(":" + port)
+	appengine.Main()
+}
+
+func insertLoginUser(db *sql.DB, mobile string, name string){
+	if _, err := db.Exec("insert into user(name, mobile) values(?, ?) on duplicate key update name = ?", name, mobile, name); err != nil{
+		log.Print("User could not be inserted: " + mobile)
+	}
+}
+
+func activateUser(db *sql.DB, mobile string){
+	if _, err := db.Exec("update user set is_active=true where mobile=?", mobile); err != nil{
+		log.Print("User could not be activated: " + mobile)
+	}
 }
 
 func getMD5Hash(text string) string {
